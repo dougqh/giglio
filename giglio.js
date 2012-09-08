63d4e4f07f944986e3bbbb3cff6a9f7eccfb09ec
Douglas Q Hawkins <dougqh@gmail.com>
2012-09-08 14:20:36 -0400

    Complete overhaul to support multiple modules -- not quite fully working yet

--- giglio.js
+++ giglio.js
@@ -9,141 +9,262 @@
  * time('foo', function(reps) {...});
  */
 
-(function(window) {
+(function(window, undefined) {
     'use strict';
-
-    var Deferred = (function() {
-        var Deferred = function(result) {
-            if ( typeof result !== 'undefined' ) {
-                this.result = result;
-            } else {
-                this._callbacks = [];
-            }
-        };
-        Deferred.prototype.success = function(fn) {
-            if ( typeof this.result !== 'undefined' ) {
-                fn(this);
-            } else {
-                this._callbacks.push(fn);
-            }
-        };
-        Deferred.prototype.set = function(result) {
-            this.result = result;
-            
-            if ( this._callbacks ) {
-                for ( var i = 0, len = this._callbacks.length; i < len; ++i ) {
-                    var callback = this._callbacks[i];
-                    callback(this);
-                }
-            }
-        };
-        Deferred.prototype.done = function() {
-            this.set(null);
-        };
-        return Deferred;
-    })();
     
+    var Deferred = function(result) {
+    	var self = {
+    		done: false,
+    		callbacks: []
+    	};
+    	
+    	var makeEvent = function() {
+    		return {
+    			result: self.result,
+    			exception: self.exception
+    		};
+    	};
+    	var markDone = function() {
+    		self.done = true;
+    		
+    		var event = makeEvent();
+    		for ( var i = 0, len = self.callbacks.length; i < len; ++i ) {
+    			try {
+    				self.callbacks[i].call(null, event);
+    			} catch ( e ) {
+    				console.log('exception in deferred callback', e);
+    			}
+    		}
+    		delete self.callbacks;
+    	};
+    	
+		var deferred = {};
+		deferred.result = function(result) {
+			self.result = result;
+			markDone();
+		};
+		deferred.exception = function(exception) {
+			self.exception = exception;
+			markDone();
+		};
+		deferred.done = function() {
+			markDone();
+		};
+		deferred.capture = function(fn, thisObj, args) {
+			thisObj = thisObj || null;
+			args = args || [];
+			
+			try {
+				this.result(fn.apply(thisObj, args));
+			} catch ( e ) {
+				this.exception(e);
+			}
+		};
+		deferred.always = function(callback) {
+		    if ( self.done ) {
+		    	callback.call(null, makeEvent());
+		    } else {
+				self.callbacks.push(callback);
+			}
+		};
+		return deferred;
+    };
+    
+    /**
+     * Frontend that writes output to the console
+     */    
     var consoleFrontend = (function() {
         var frontend = {};
         
         frontend.module = function(module) {
             console.log('Benchmarking ' + module.name + '...');
         };
-        frontend.startTime = function(module, entry) {
-            console.time(entry.name);
+        frontend.success = function(module, funcEntry, timeMs) {
+        	console.log(module.name + ' ' + funcEntry.name + ' ' + timeMs + 'ms');
         };
-        frontend.endTime = function(module, entry) {
-            console.timeEnd(entry.name);
+        frontend.failure = function(module, funcEntry, exception) {
+        	console.log(module.name + ' ' + funcEntry.name, exception);
         };
         
         return frontend;
     })();
     
-    var timerBackend = (function() {        
-        var schedule = function(fn) {
-            window.setTimeout(fn, 0);
-        };
-        
-        var backend = {};
-        backend.benchmark = function(frontend, module, reps) {
-            frontend.module(module);
-            
-            var remainingEntries = module.funcEntries.slice(0, module.funcEntries.length);
-            
-            var timeNext = function() {
-                if ( ! remainingEntries.length ) {
-                    return;
-                }
-                
-                var entry = remainingEntries.shift();
-                
-                var deferred = backend.time(frontend, module, entry, reps);
-                deferred.success(timeNext);
-            };
-            schedule(timeNext);
-        };
-        backend.time = function(frontend, module, entry, reps) {
-            var deferred = new Deferred();
-            
-            var timeIt = function() {
-                var context = {};
-                
-                module.lifecycle.setup.call(context);
-                try {
-                    frontend.startTime(module, entry);
-                    
-                    entry.func.call(context, reps);
-                
-                    frontend.endTime(module, entry);
-                    
-                    deferred.done();
-                } finally {
-                    module.lifecycle.teardown.call(context);
-                }
-            };
-            
-            schedule(timeIt);
-            return deferred;
-        };
-        
-        return backend;
+    
+    /**
+     * Timer that uses built-in console.time and timeEnd functions.
+     * (not supported in all browsers)
+     * Does not return a value so it is not compatible with most frontends.
+     */
+    var consoleTimer = (function() {
+    	var timer = {};
+    	
+    	timer.startTime = function(module, entry) {
+    		//console.time(module.name + ' ' + entry.name);
+    	};
+    	
+    	timer.endTime = function(module, entry) {
+    		//console.timeEnd(module.name + ' ' + entry.name);
+    	};
+    });
+    
+    
+    /**
+     * Executor that uses window.setTimeout to schedule timing runs
+     */
+    var timeoutExecutor = (function() {
+    	var executor = {};
+    	executor.schedule = function(fn, thisObj, args) {
+    		var deferred = Deferred();
+    		window.setTimeout(function() {
+    			deferred.capture(fn, thisObj, args);
+    		}, 0);
+    		return deferred;
+    	};
+    	return executor;
     })();
     
-    var giglio = (function() {
-        var config = {
-            frontend: consoleFrontend,
-            backend: timerBackend
-        };
+    /**
+     * Immediate executor that runs right now
+     */
+    var immediateExecutor = (function() {
+    	var executor = {};
+    	executor.schedule = function(fn, thisObj, args) {
+    		fn.apply(thisObj, args);
+    	};
+    	return executor;
+    })();
     
+    
+    /**
+     * Engine that drives the running of the time functions
+     */
+    var engine = (function() {
+    	var process = function(executor, array, deferredFn) {
+    		var overallDeferred = Deferred();
+    		
+    		var queue = array.slice(0, array.length);
+    		
+    		var next, scheduleNext;
+    		scheduleNext = function() {
+    		    if ( queue.length === 0 ) {
+    				overallDeferred.done();
+    			} else {
+    				executor.schedule(next);
+    			}
+    		};
+    		next = function(deferred) {
+    			var deferred = deferredFn(queue.shift());
+    			deferred.always(scheduleNext);
+    		};
+    		scheduleNext();
+    		
+    		return overallDeferred;
+    	};
+    	
+    	var engine = {};
+    	
+    	engine.timeAll = function(config, reps, modules) {
+    		return process(config.executor, modules, function(module) {
+    			engine.timeModule(config, reps, module);
+    		});
+    	};
+    	
+    	engine.timeModule = function(config, reps, module) {
+    		config.frontend.module(module);
+    		
+    	    return process(config.executor, module.funcEntries, function(funcEntry) {
+    	    	var deferred = Deferred();
+    	    	deferred.capture(function() {
+    	    		engine.timeFunction(config, reps, module, funcEntry);
+    	    	});
+    	    	return deferred;
+    	    });
+    	};
+    	
+    	engine.timeFunction = function(config, reps, module, funcEntry) {
+    		var context = {};
+    		
+    		module.setup.call(context);
+			try {
+				console.time(funcEntry.name);
+				//config.timer.startTime(module, funcEntry);
+			
+				var exception;
+				var result;
+				try {
+					funcEntry.func.call(context, reps);
+				} catch ( e ) {
+					exception = e;
+				} finally {
+					console.timeEnd(funcEntry.name);
+					//result = config.timer.endTime(module, funcEntry);
+				}
+				
+				// truthiness is insufficient because the timing could 
+				// potentially be 0ms
+				if ( typeof result !== 'undefined' ) {
+					config.frontend.success(module, funcEntry, result);
+				} else if ( typeof exception !== 'undefined' ) {
+					config.frontend.failure(module, funcEntry, exception);
+				}
+			} catch ( e ) {
+				config.frontend.error(module, funcEntry, e);
+				throw e;
+			} finally {
+				module.teardown.call(context);
+			}    			
+    	};
+    	
+    	return engine;
+    })();
+    
+    
+    /**
+     * The actual exposed API
+     */
+    var giglio = (function() {
         var giglio = {};
         
         giglio.Deferred = Deferred;
         giglio.consoleFrontend = consoleFrontend;
-        giglio.timerBackend = timerBackend;
+        giglio.consoleTimer = consoleTimer;
+        giglio.timeoutExecutor = timeoutExecutor;
+        giglio.immediateExecutor = immediateExecutor;
         
-        var module;
+        var config = {
+            frontend: giglio.consoleFrontend,
+            timer: giglio.consoleTimer,
+            executor: timeoutExecutor
+        };
+        
+        var modules = [];
         
         var nop = function() {};
         
-        giglio.module = function(name, lifecycle) {
-            module = {
+        giglio.module = function(name, options) {
+            var module = {
                 name: name,
-                lifecycle: {
-                    setup: lifecycle.setup || nop,
-                    teardown: lifecycle.teardown || nop
-                },
+                setup: nop,
+                teardown: nop,
                 funcEntries: []
             };
+            
+            if ( options ) {
+            	module.setup = options.setup || module.setup;
+            	module.teardown = options.teardown || module.teardown;
+            }
+            
+            modules.push(module);
         };
         giglio.time = function(name, fn) {
-            if ( ! module ) {
+        	if ( modules.length === 0 ) {
                 giglio.module('Benchmark');
             }
             
-            module.funcEntries.push({name: name, func: fn});
+            modules[modules.length-1].funcEntries.push({name: name, func: fn});
         };
         giglio.benchmark = function(reps) {
-            config.backend.benchmark(config.frontend, module, reps);
+            engine.timeAll(config, reps, modules);
         };
         
         return giglio;
@@ -154,4 +275,4 @@
     window.module = window.module || giglio.module;
     window.time = window.time || giglio.time;
     window.benchmark = window.benchmark || giglio.benchmark;
-})(window);
+})( (function() {return this;})() );
